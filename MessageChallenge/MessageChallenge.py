606e476655d8d71feecbac9a9b0420f4de8f1bea
Gerardo Cabrera <gerardojaviercr@gmail.com>
2021-03-14 18:15:45 -0600

    Read file

--- MessageChallenge/MessageChallenge.py
+++ MessageChallenge/MessageChallenge.py
@@ -1,3 +1,9 @@
 
 f = open("D:/Mensaje.txt", "r")
-print(f.read())
+
+lengths = f.readline().strip()
+instruction1 = f.readline().strip()
+instruction2 = f.readline().strip()
+message = f.readline().strip()
+
+print(instruction1, instruction2, message)

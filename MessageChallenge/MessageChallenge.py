b634273bbd01f8eaf4d726da9414e00e81d89b10
Gerardo Cabrera <gerardojaviercr@gmail.com>
2021-03-17 12:35:05 -0600

    Added file and destination selector

--- MessageChallenge/MessageChallenge.py
+++ MessageChallenge/MessageChallenge.py
@@ -1,14 +1,24 @@
 import re
+from tkinter.filedialog import askopenfilename
+from tkinter import filedialog
 
-f = open("D:/Mensaje.txt", "r")
+filename = askopenfilename()
+if(filename != "" or filename != null):
+    f = open(filename, "r")
 
-lengths = f.readline().strip()
-instruction1 = f.readline().strip()
-instruction2 = f.readline().strip()
-message = f.readline().strip()
+    lengths = f.readline().strip()
+    instruction1 = f.readline().strip()
+    instruction2 = f.readline().strip()
+    message = f.readline().strip()
 
-message = re.sub(r'(.)\1+', r'\1', message)
+    message = re.sub(r'(.)\1+', r'\1', message)
 
-f = open("D:/Mensaje-result.txt", "w")
-f.write("%s\n%s" %(("Si" if instruction1 in message else "No"), ("Si" if instruction2 in message else "No")))
-f.close()
+    directory = filedialog.askdirectory()
+    if(directory != "" or directory != null):
+        f = open(directory + "/Mensaje-result.txt", "w")
+        f.write("%s\n%s" %(("Si" if instruction1 in message else "No"), ("Si" if instruction2 in message else "No")))
+        f.close()
+    else:
+        print("No path selection")
+else:
+    print("No selection")
